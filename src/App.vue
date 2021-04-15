2dafff187c8c5b0a838d696138aa25469d80a72a
何东树 <hedongshu@egret.com>
2021-04-15 16:46:17 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,92 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <div>
+      <input
+        type="file"
+        name=""
+        id=""
+      >
+    </div>
+
+    <div>
+      <!-- 经验公式 -->
+      <div>
+        <check-component
+          span='经验公式'
+          :value='0'
+          v-model="conversionType"
+        />
+
+      </div>
+
+      <!-- 校正系数 -->
+      <div>
+        <check-component
+          span='校正系数'
+          :value='1'
+          v-model="conversionType"
+        />
+
+        <div>
+          <h3>数据采集类型</h3>
+
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <button type="button">开始转换</button>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, reactive, ref, watch } from "vue";
+import CheckComponent from "@/components/Check.vue";
 
-@Options({
+interface AppConfig {
+  conversion_type: string;
+}
+
+enum conversionTypeEnum {
+  "经验公式",
+  "校正系数",
+}
+enum collectTypeEnum {
+  "LAI-2200",
+  "DHP",
+  "TRAC",
+}
+enum treeTypeEnum {
+  "阔叶林",
+  "针叶林",
+  "针阔混交",
+}
+
+export default defineComponent({
   components: {
-    HelloWorld,
+    CheckComponent,
   },
-})
-export default class App extends Vue {}
+  setup() {
+    const conversionType = ref<conversionTypeEnum>(
+      conversionTypeEnum["经验公式"]
+    );
+    const collectType = ref<collectTypeEnum>(collectTypeEnum["LAI-2200"]);
+    const treeType = ref<treeTypeEnum>(treeTypeEnum["阔叶林"]);
+
+    watch(conversionType, (value) => {
+      console.log(value);
+    });
+
+    return {
+      conversionType,
+      collectType,
+      treeType,
+    };
+  },
+  computed: {},
+});
 </script>
 
 <style lang="scss">
@@ -23,8 +94,6 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Check.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      :name="span"
+      :id="span"
+      :value="value"
+      v-model="_modelValue"
+    >
+    <label :for="span">{{span}}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    span: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: [Number, String],
+      required: true,
+    },
+    modelValue: [Number, String],
+  },
+
+  setup(props) {
+    console.log(props);
+  },
+
+  computed: {
+    _modelValue: {
+      get(): string | number {
+        return this.modelValue as string | number;
+      },
+      set(value: string | number): void {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
